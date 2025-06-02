b22edbe181cca4190083ce0f68a7c1cb2c129188
Subhan <sheikhanwer30@gmail.com>
2025-06-03 00:27:44 +0500

    home page images and links

--- components/home-page/FeaturedSection.tsx
+++ components/home-page/FeaturedSection.tsx
@@ -1,12 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const FeaturedSection = () => {
   const discountCards = [
-    { img: "/premium-ring.jpg", name: "Premium Ring", price: 120, old: 140 },
-    { img: "/necklace.jpg", name: "Necklace", price: 120, old: 140 },
-    { img: "/bracelet.jpg", name: "Bracelet", price: 130, old: 160 },
-    { img: "/cufflink.jpg", name: "Cuff links", price: 135, old: 150 },
+    {
+      img: "/f-ring.jpg",
+      name: "Premium Ring",
+      price: 120,
+      old: 140,
+      link: "https://classystyle.vercel.app/products/eternal-bond-name-rings-personalized-for-him-and-her",
+    },
+    {
+      img: "/f-necklace.jpg",
+      name: "Necklace",
+      price: 120,
+      old: 140,
+      link: "https://classystyle.vercel.app/products/name-glow-custom-necklace",
+    },
+    {
+      img: "/f-bracelet.jpg",
+      name: "Bracelet",
+      price: 130,
+      old: 160,
+      link: "https://classystyle.vercel.app/products/name-bloom-custom-bracelet",
+    },
+    {
+      img: "/f-cufflink.jpg",
+      name: "Cuff links",
+      price: 135,
+      old: 150,
+      link: "https://classystyle.vercel.app/products/classic-monogram-cufflinks",
+    },
   ];
 
   return (
@@ -17,6 +42,7 @@ const FeaturedSection = () => {
 
       <div className="flex flex-col lg:flex-row gap-12 w-full max-w-7xl">
         {/* Long Image */}
+        
         <div className="flex flex-col lg:w-[40%] w-full h-auto">
           <Image
             src={"/necklace-2.jpg"}
@@ -44,26 +70,28 @@ const FeaturedSection = () => {
         {/* Cards */}
         <div className="w-full lg:w-[55%] grid grid-cols-1 sm:grid-cols-2 flex-wrap gap-6 sm:gap-8 ">
           {discountCards.map((item, i) => (
-            <div key={i} className="flex flex-col">
-              <Image
-                src={item.img}
-                alt={item.name}
-                className="w-full h-auto rounded-[10px] object-cover object-center mb-3 "
-                width={300}
-                height={300}
-              />
-              <h5 className="font-playfair text-[28px] md:text-3xl font-normal">
-                {item.name}
-              </h5>
-              <div className="flex gap-2.5 items-center mt-0 md:mt-3">
-                <p className="font-inter text-lg md:text-xl tracking-[1px]">
-                  SAR {item.price}
-                </p>
-                <p className="font-inter text-sm md:text-base text-[#747474] line-through">
-                  SAR {item.old}
-                </p>
+            <Link key={i} href={item.link}>
+              <div className="flex flex-col">
+                <Image
+                  src={item.img}
+                  alt={item.name}
+                  className="w-full h-auto rounded-[10px] object-cover object-center mb-3 "
+                  width={300}
+                  height={300}
+                />
+                <h5 className="font-playfair text-[28px] md:text-3xl font-normal">
+                  {item.name}
+                </h5>
+                <div className="flex gap-2.5 items-center mt-0 md:mt-3">
+                  <p className="font-inter text-lg md:text-xl tracking-[1px]">
+                    SAR {item.price}
+                  </p>
+                  <p className="font-inter text-sm md:text-base text-[#747474] line-through">
+                    SAR {item.old}
+                  </p>
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
