4938c595678a88277c424bd0afae42e21d73ab35
Subhan <sheikhanwer30@gmail.com>
2025-07-07 13:19:45 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  authorizedParties: ['https://classystyle.net', 'https://classystyle.vercel.app'],
+})
 
 export const config = {
   matcher: [
